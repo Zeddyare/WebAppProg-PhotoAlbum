9a84c5970b3b430dd64422afcc73fc461a31f16c
Zeddyare <w0433704@nscc.ca>
2025-09-17 10:18:35 -0300

    Added Category Class

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace PhotoAlbum.Models
+{
+    public class Category
+    {
+        //Primary key
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        //Navigation property
+        public List<Photo>? Photos { get; set; } 
+
+    }
+}
